d7a92cac2b9a0b268791d0dd74bb6b5adf5c0027
brectanus <brectanus@9017d574-64ec-4062-9424-5e00b32a252b>
2007-08-22 20:12:41 +0000

    Adjust hook placement so mod_breach_trans fixes the request before us.

--- apache2/mod_security2.c
+++ apache2/mod_security2.c
@@ -1028,6 +1028,7 @@ static void register_hooks(apr_pool_t *mp) {
         "mod_rpaf.c",
         "mod_extract_forwarded2.c",
         "mod_breach_realip.c",
+        "mod_breach_trans.c",
         "mod_unique_id.c",
         NULL
     };
