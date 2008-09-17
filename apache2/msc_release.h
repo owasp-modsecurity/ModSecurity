be4e8438ddce8be761e6e79e8394ac3672ba90d7
brectanus <brectanus@9017d574-64ec-4062-9424-5e00b32a252b>
2008-09-17 17:00:00 +0000

    Update versions for tagging 2.5.7-rc1.

--- apache2/msc_release.h
+++ apache2/msc_release.h
@@ -46,10 +46,10 @@ typedef struct modsec_build_type_rec {
 extern DSOLOCAL modsec_build_type_rec modsec_build_type[];
 
 #define MODSEC_VERSION_MAJOR       "2"
-#define MODSEC_VERSION_MINOR       "6"
-#define MODSEC_VERSION_MAINT       "0"
-#define MODSEC_VERSION_TYPE        "-trunk"
-#define MODSEC_VERSION_RELEASE     ""
+#define MODSEC_VERSION_MINOR       "5"
+#define MODSEC_VERSION_MAINT       "7"
+#define MODSEC_VERSION_TYPE        "-rc"
+#define MODSEC_VERSION_RELEASE     "1"
 
 #define MODSEC_VERSION_SUFFIX MODSEC_VERSION_TYPE MODSEC_VERSION_RELEASE
 
