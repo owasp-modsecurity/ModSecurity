f7f305991fb65a3faa08947e91096fffb63e7f0b
b1v1r <b1v1r@9017d574-64ec-4062-9424-5e00b32a252b>
2010-05-05 23:00:57 +0000

    Add license headers to extension examples.

--- ext/mod_var_remote_addr_port.c
+++ ext/mod_var_remote_addr_port.c
@@ -1,3 +1,21 @@
+/*
+ * ModSecurity for Apache 2.x, http://www.modsecurity.org/
+ * Copyright (c) 2004-2010 Breach Security, Inc. (http://www.breach.com/)
+ *
+ * This product is released under the terms of the General Public Licence,
+ * version 2 (GPLv2). Please refer to the file LICENSE (included with this
+ * distribution) which contains the complete text of the licence.
+ *
+ * There are special exceptions to the terms and conditions of the GPL
+ * as it is applied to this software. View the full text of the exception in
+ * file MODSECURITY_LICENSING_EXCEPTION in the directory of this software
+ * distribution.
+ *
+ * If any of the files related to licensing are missing or if you have any
+ * other questions related to licensing please contact Breach Security, Inc.
+ * directly using the email address support@breach.com.
+ *
+ */
 
 #include "httpd.h"
 #include "http_core.h"
