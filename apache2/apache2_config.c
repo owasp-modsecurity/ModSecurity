86cc9d6e31dccc1559cfa161db4429a9a7b4b47b
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2010-12-23 16:06:45 +0000

    Request process partial under detectiononly

--- apache2/apache2_config.c
+++ apache2/apache2_config.c
@@ -1558,6 +1558,7 @@ static const char *cmd_rule_engine(cmd_parms *cmd, void *_dcfg, const char *p1)
     if (strcasecmp(p1, "detectiononly") == 0) {
         dcfg->is_enabled = MODSEC_DETECTION_ONLY;
         dcfg->of_limit_action = RESPONSE_BODY_LIMIT_ACTION_PARTIAL;
+        dcfg->if_limit_action = REQUEST_BODY_LIMIT_ACTION_PARTIAL;
     } else
     return apr_psprintf(cmd->pool, "ModSecurity: Invalid value for SecRuleEngine: %s", p1);
 
