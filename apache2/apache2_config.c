7635bd5f1a03d1bb904c5251895b4994d5aab65e
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2011-04-07 19:04:13 +0000

    Improvements in detection only

--- apache2/apache2_config.c
+++ apache2/apache2_config.c
@@ -1750,6 +1750,11 @@ static const char *cmd_resquest_body_limit_action(cmd_parms *cmd, void *_dcfg,
     directory_config *dcfg = (directory_config *)_dcfg;
     if (dcfg == NULL) return NULL;
 
+    if (dcfg->is_enabled == MODSEC_DETECTION_ONLY)  {
+        dcfg->if_limit_action = REQUEST_BODY_LIMIT_ACTION_PARTIAL;
+        return NULL;
+    }
+
     if (strcasecmp(p1, "ProcessPartial") == 0) dcfg->if_limit_action = REQUEST_BODY_LIMIT_ACTION_PARTIAL;
     else
     if (strcasecmp(p1, "Reject") == 0) dcfg->if_limit_action = REQUEST_BODY_LIMIT_ACTION_REJECT;
