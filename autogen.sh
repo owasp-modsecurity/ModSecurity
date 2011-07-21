1abe50a576fff54972331259bdd4650ca64253d0
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2011-07-21 15:05:15 +0000

    Fixes on autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
-rm -rf autom4te.cache
+#rm -rf autom4te.cache
+#automake --add-missing --copy --foreign
+#autoreconf --install
+#autoheader
 
-automake --add-missing --copy --foreign
+rm -rf autom4te.cache
+rm -f aclocal.m4
+libtoolize --force --copy
 autoreconf --install
 autoheader
+automake --add-missing --foreign --copy --force-missing
+autoconf --force
+rm -rf autom4te.cache
+
