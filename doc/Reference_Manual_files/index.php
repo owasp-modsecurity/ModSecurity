02e1cac82d60d1b9bbf8b3b346f87a04c928ce06
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2011-12-19 12:53:29 +0000

    Update doc/

---
+++ doc/Reference_Manual_files/index.php
@@ -0,0 +1,8 @@
+/* generated javascript */
+var skin = 'sourceforge';
+var stylepath = '/apps/mediawiki/mod-security/skins';
+
+/* MediaWiki:Common.js */
+/* Any JavaScript here will be loaded for all users on every page load. */
+
+/* MediaWiki:Sourceforge.js */
