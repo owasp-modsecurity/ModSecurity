f0fab2a80394df9f97b4b3caeed5f33d042d4c02
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2012-05-14 23:08:11 +0000

    Fix apache 2.4 compilation issue during make test

--- apache2/modsecurity.c
+++ apache2/modsecurity.c
@@ -125,6 +125,7 @@ int modsecurity_init(msc_engine *msce, apr_pool_t *mp) {
         return -1;
     }
 
+#if !defined(MSC_TEST)
 #ifdef __SET_MUTEX_PERMS
 #if AP_SERVER_MAJORVERSION_NUMBER > 1 && AP_SERVER_MINORVERSION_NUMBER > 2
     rc = ap_unixd_set_global_mutex_perms(msce->auditlog_lock);
@@ -153,7 +154,7 @@ int modsecurity_init(msc_engine *msce, apr_pool_t *mp) {
         return -1;
     }
 #endif /* SET_MUTEX_PERMS */
-
+#endif
 
     return 1;
 }
