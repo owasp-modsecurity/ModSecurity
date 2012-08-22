c9202a44b204519ac3d70aecec75cfdeac76843d
brenosilva <brenosilva@9017d574-64ec-4062-9424-5e00b32a252b>
2012-08-22 14:05:07 +0000

    Update license

--- iis/mymodule.h
+++ iis/mymodule.h
@@ -1,3 +1,17 @@
+/*
+* ModSecurity for Apache 2.x, http://www.modsecurity.org/
+* Copyright (c) 2004-2011 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+*
+* You may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* If any of the files related to licensing are missing or if you have any
+* other questions related to licensing please contact Trustwave Holdings, Inc.
+* directly using the email address security@modsecurity.org.
+*/
+
 #ifndef __MY_MODULE_H__
 #define __MY_MODULE_H__
 
@@ -8,35 +22,35 @@
 class CMyHttpModule : public CHttpModule
 {
 public:
-	HANDLE	m_hEventLog;
-	DWORD	m_dwPageSize;
+    HANDLE  m_hEventLog;
+    DWORD   m_dwPageSize;
 
-	REQUEST_NOTIFICATION_STATUS
-	OnBeginRequest(
-		IN IHttpContext * pHttpContext,
-		IN IHttpEventProvider * pProvider
-	);
+    REQUEST_NOTIFICATION_STATUS
+    OnBeginRequest(
+        IN IHttpContext * pHttpContext,
+        IN IHttpEventProvider * pProvider
+    );
 
-	REQUEST_NOTIFICATION_STATUS
-	OnSendResponse(
-		IN IHttpContext * pHttpContext,
-	    IN ISendResponseProvider * pProvider
-	);
+    REQUEST_NOTIFICATION_STATUS
+    OnSendResponse(
+    IN IHttpContext * pHttpContext,
+        IN ISendResponseProvider * pProvider
+    );
 
-	REQUEST_NOTIFICATION_STATUS
-	OnPostEndRequest(
-		IN IHttpContext * pHttpContext,
-		IN IHttpEventProvider * pProvider
-	);
+    REQUEST_NOTIFICATION_STATUS
+    OnPostEndRequest(
+    IN IHttpContext * pHttpContext,
+    IN IHttpEventProvider * pProvider
+    );
 
-	HRESULT ReadFileChunk(HTTP_DATA_CHUNK *chunk, char *buf);
-	
-	CMyHttpModule();
-	~CMyHttpModule();
+    HRESULT ReadFileChunk(HTTP_DATA_CHUNK *chunk, char *buf);
 
-	void Dispose();
+    CMyHttpModule();
+    ~CMyHttpModule();
 
-	BOOL WriteEventViewerLog(LPCSTR szNotification, WORD category = EVENTLOG_INFORMATION_TYPE);
+    void Dispose();
+
+    BOOL WriteEventViewerLog(LPCSTR szNotification, WORD category = EVENTLOG_INFORMATION_TYPE);
 };
 
 #endif
--- nginx/modsecurity/ngx_http_modsecurity_module.c
+++ nginx/modsecurity/ngx_http_modsecurity_module.c
@@ -1,3 +1,17 @@
+/*
+* ModSecurity for Apache 2.x, http://www.modsecurity.org/
+* Copyright (c) 2004-2011 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+*
+* You may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* If any of the files related to licensing are missing or if you have any
+* other questions related to licensing please contact Trustwave Holdings, Inc.
+* directly using the email address security@modsecurity.org.
+*/
+
 #include <nginx.h>
 #include <ngx_config.h>
 #include <ngx_core.h>
