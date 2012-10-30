532a82890196e68cb23f0d8140d7705eda96a248
Breno Silva <breno.silva@gmail.com>
2012-10-30 18:18:38 -0400

    Change names of HMAC feature to HASH

--- apache2/msc_crypt.h
+++ apache2/msc_crypt.h
@@ -27,11 +27,6 @@
 #define INT32_MAX      (2147483647)
 #endif
 
-#define N16BITS_MASK 65536L
-#define N16BITS_MAX  0xFFFF
-#define N15BITS_MASK 0x7FFF
-#define N31BITS_MASK 0x7FFFFFFF
-
 char DSOLOCAL *hmac(modsec_rec *msr, const char *key, int key_len,
                      unsigned char *msg, int msglen);
 char DSOLOCAL *do_hash_link(modsec_rec *msr, char *link,
@@ -39,9 +34,9 @@ char DSOLOCAL *do_hash_link(modsec_rec *msr, char *link,
 char DSOLOCAL *getkey(apr_pool_t *mp);
 
 int DSOLOCAL init_response_body_html_parser(modsec_rec *msr);
-int DSOLOCAL encrypt_response_body_links(modsec_rec *msr);
-int DSOLOCAL inject_encrypted_response_body(modsec_rec *msr, int elts);
-int DSOLOCAL do_encryption_method(modsec_rec *msr, char *link, int type);
+int DSOLOCAL hash_response_body_links(modsec_rec *msr);
+int DSOLOCAL inject_hashed_response_body(modsec_rec *msr, int elts);
+int DSOLOCAL do_hash_method(modsec_rec *msr, char *link, int type);
 int DSOLOCAL modify_response_header(modsec_rec *msr);
 char DSOLOCAL *normalize_path(modsec_rec *msr, char *input);
 #endif
