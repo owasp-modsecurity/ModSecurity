9027fbbfdf9ab2d81c2cf99a7f3038a6845a4837
Breno Silva <breno.silva@gmail.com>
2012-10-30 20:10:19 -0400

    Update CHANGES and doc/

--- doc/Reference_Manual_files/index.php
+++
@@ -1,8 +0,0 @@
-/* generated javascript */
-var skin = 'sourceforge';
-var stylepath = '/apps/mediawiki/mod-security/skins';
-
-/* MediaWiki:Common.js */
-/* Any JavaScript here will be loaded for all users on every page load. */
-
-/* MediaWiki:Sourceforge.js */
