ee47cccb63588fef6294351abdf07e7b914404d9
chaizhenhua <chaizhenhua@gmail.com>
2013-01-04 23:10:05 +0800

    Fixed request body is larger

--- nginx/modsecurity/ngx_http_modsecurity.c
+++ nginx/modsecurity/ngx_http_modsecurity.c
@@ -384,7 +384,7 @@ modsecurity_read_body_cb(request_rec *r, char *outpos, unsigned int length,
             
             outpos = (char *) ngx_cpymem(outpos, buf->pos, len);
             rest -= len;
-            buf->last += len;
+            buf->pos += len;
         } else if (buf->in_file) {
             
             size = ngx_read_file(buf->file, (u_char*)outpos, len, buf->file_pos);
@@ -566,7 +566,7 @@ ngx_http_modsecurity_handler(ngx_http_request_t *r)
         /* hijack loc_conf so that we can receive any body length
          *  TODO: nonblocking process & chuncked body
          */
-        if (clcf->client_body_buffer_size < r->headers_in.content_length_n) {
+        if (clcf->client_body_buffer_size < (size_t)r->headers_in.content_length_n) {
             
             loc_conf = ngx_pcalloc(r->pool, sizeof(void *) * ngx_http_max_module);
             if (loc_conf == NULL) {
