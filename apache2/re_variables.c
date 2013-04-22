6cd3a14db1e4a70e69caafec33f3a61a93b6874e
Breno Silva <breno.silva@gmail.com>
2013-04-22 13:36:13 -0400

    MODSEC-362

--- apache2/re_variables.c
+++ apache2/re_variables.c
@@ -700,13 +700,41 @@ static int var_useragent_ip_generate(modsec_rec *msr, msre_var *var, msre_rule *
 static int var_remote_addr_generate(modsec_rec *msr, msre_var *var, msre_rule *rule,
     apr_table_t *vartab, apr_pool_t *mptmp)
 {
-    return var_simple_generate(var, vartab, mptmp, msr->remote_addr);
+    char *remote = NULL;
+    char *parse_remote = NULL;
+    char *saved = NULL;
+    char *str = NULL;
+
+    if(strcasecmp(msr->txcfg->remote_define, "default") == 0)   {
+        if (msr->txcfg->debuglog_level >= 9) {
+            msr_log(msr, 9, "Set variable \"%s\" to \"%s\".", var->name, msr->remote_addr);
+        }
+        return var_simple_generate(var, vartab, mptmp, msr->remote_addr);
+    } else  {
+        remote = (char *)apr_table_get(msr->r->headers_in, msr->txcfg->remote_define);
+        if(remote == NULL)  {
+            if (msr->txcfg->debuglog_level >= 9) {
+                msr_log(msr, 9, "Request header \"%s\" not present setting variable \"%s\" to \"%s\".", msr->txcfg->remote_define,
+                    var->name, msr->remote_addr);
+            }
+            return var_simple_generate(var, vartab, mptmp, msr->remote_addr);
+        } else  {
+            parse_remote = apr_pstrdup(msr->mp, remote);
+            str = apr_strtok(parse_remote, ",", &saved);
+            msr->remote_addr = apr_pstrdup(msr->mp, str);
+            if (msr->txcfg->debuglog_level >= 9) {
+                msr_log(msr, 9, "Request header \"%s\" is present setting variable \"%s\" to \"%s\".", msr->txcfg->remote_define,
+                    var->name, msr->remote_addr);
+            }
+            return var_simple_generate(var, vartab, mptmp, msr->remote_addr);
+        }
+    }
 }
 
 /* REMOTE_HOST */
 
 static int var_remote_host_generate(modsec_rec *msr, msre_var *var, msre_rule *rule,
-    apr_table_t *vartab, apr_pool_t *mptmp)
+        apr_table_t *vartab, apr_pool_t *mptmp)
 {
     const char *value1 = ap_get_remote_host(msr->r->connection, msr->r->per_dir_config,
         REMOTE_NAME, NULL);
