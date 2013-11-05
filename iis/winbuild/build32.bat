928dc68740bcfc698161e422e0ff7301683c30f6
Felipe Zimmerle <fcosta@trustwave.com>
2013-11-05 09:50:04 -0300

    iss: Changes httpd version 2.4.6
    Apache version was changed to 2.4.6 to sync with the current
    apache lounge version.

--- iis/winbuild/build32.bat
+++ iis/winbuild/build32.bat
@@ -1,5 +1,6 @@
-SET APACHE24_ZIP=httpd-2.4.3-win32.zip
+SET APACHE24_ZIP=httpd-2.4.6-win32.zip
 mkdir %DROP%
-call vsvars32.bat
+rem call vcvars32.bat
+call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\vcvars32.bat"
 call init.bat
 call build.bat
