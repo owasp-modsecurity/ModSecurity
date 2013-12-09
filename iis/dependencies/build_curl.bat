1f15d85536486d9682a21d59efd6e294160791ef
Felipe "Zimmerle" Costa <fcosta@trustwave.com>
2013-12-09 20:45:41 -0200

    iis: Waiting for 5 seconds before move curl directory
    Testing buildbot.

--- iis/dependencies/build_curl.bat
+++ iis/dependencies/build_curl.bat
@@ -1,15 +1,18 @@
 cd "%WORK_DIR%"
 
 @if NOT EXIST "%SOURCE_DIR%\%CURL%" goto file_not_found_bin
-
+echo "7z..."
 7z.exe x "%SOURCE_DIR%\%CURL%"
-
+echo "Timeout..."
+timeout 5
+echo "Curl..."
 set CURL_DIR=%CURL:~0,-4%
-
+echo "Move..."
 move "%CURL_DIR%" "curl"
-
+echo "Cd..."
 :: copy /y CMakeLists.txt "curl"
 CD "curl"
+echo "Cmake..."
 CMAKE   -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_SHARED_LIBS=True -DCURL_ZLIB=True
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
 :: "%WORK_DIR%\fart.exe" -r -C "%WORK_DIR%\curl\include\curl\curlbuild.h" LLU ULL
--- iis/dependencies/build_ssdeep.bat
+++ iis/dependencies/build_ssdeep.bat
@@ -1,33 +1,45 @@
 cd "%WORK_DIR%"
 
+echo "Checking for file... %SOURCE_DIR%\%SSDEEP%"
 @if NOT EXIST "%SOURCE_DIR%\%SSDEEP%" goto build_failed
 
+echo "7z..."
 @7z.exe x "%SOURCE_DIR%\%SSDEEP_BIN%"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
+echo "7z..."
 @7z.exe x "%SOURCE_DIR%\%SSDEEP%" -so | 7z.exe x -aoa -si -ttar 
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
 
+echo "Set SSDEEP_DIR..."
 set SSDEEP_DIR=%SSDEEP_BIN:~0,-4%
 
+echo "Move..."
 move "%SSDEEP_DIR%" "ssdeep"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
+echo "Cd..."
 cd "%WORK_DIR%\ssdeep\"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
 
+echo "Set SSDEEP_ARCH..."
 @set SSDEEP_ARCH="x86"
 @call cl 2>&1 | findstr /C:"x64"
 @if (%ERRORLEVEL%) == (0) set SSDEEP_ARCH="x64"
 
+echo "lib..."
 lib /machine:%SSDEEP_ARCH% /def:fuzzy.def
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
 
+echo "copy..."
 copy /y "%WORK_DIR%\ssdeep\fuzzy.dll" "%OUTPUT_DIR%"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
+echo "copy..."
 copy /y "%WORK_DIR%\ssdeep\fuzzy.def" "%OUTPUT_DIR%"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
+echo "copy..."
 copy /y "%WORK_DIR%\ssdeep\fuzzy.lib" "%OUTPUT_DIR%"
 @if NOT (%ERRORLEVEL%) == (0) goto build_failed
 
+echo "done"
 
 @exit /B 0
 
