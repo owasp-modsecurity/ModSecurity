ea8d9c0e82ca3ea9aa2f1a8d6f44af1779084dc7
Felipe Zimmerle <fcosta@trustwave.com>
2017-02-26 23:32:48 -0300

    First version of the inline highlight calculation

--- src/rule.cc
+++ src/rule.cc
@@ -358,8 +358,7 @@ std::list<std::pair<std::shared_ptr<std::string>,
                 if (multiMatch == true) {
                     if (*newValue != *value) {
                         ret.push_back(std::make_pair(
-                            newValue,
-                                          transStr));
+                            newValue, transStr));
                     }
                 }
                 value = std::shared_ptr<std::string>(newValue);
@@ -388,8 +387,7 @@ std::list<std::pair<std::shared_ptr<std::string>,
             if (multiMatch == true) {
                 if (*value != *newValue) {
                     ret.push_back(std::make_pair(
-                        newValue,
-                                      transStr));
+                        newValue, transStr));
                     value = newValue;
                 }
             }
