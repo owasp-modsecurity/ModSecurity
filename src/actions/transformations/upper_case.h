37868d153412d5d1863502f306464654adb07381
Victor Hora <vhora@trustwave.com>
2017-06-02 02:05:08 -0400

    Add missing feature: t:uppercase transformation

---
+++ src/actions/transformations/upper_case.h
@@ -0,0 +1,46 @@
+/*
+ * ModSecurity, http://www.modsecurity.org/
+ * Copyright (c) 2015 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+ *
+ * You may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * If any of the files related to licensing are missing or if you have any
+ * other questions related to licensing please contact Trustwave Holdings, Inc.
+ * directly using the email address security@modsecurity.org.
+ *
+ */
+
+#include <string>
+#include <unordered_map>
+
+#include "modsecurity/actions/action.h"
+#include "src/actions/transformations/transformation.h"
+
+#ifndef SRC_ACTIONS_TRANSFORMATIONS_UPPER_CASE_H_
+#define SRC_ACTIONS_TRANSFORMATIONS_UPPER_CASE_H_
+
+#ifdef __cplusplus
+
+namespace modsecurity {
+class Transaction;
+namespace actions {
+namespace transformations {
+
+
+class UpperCase : public Transformation {
+ public:
+    explicit UpperCase(std::string action);
+    std::string evaluate(std::string exp,
+        Transaction *transaction) override;
+};
+
+}  // namespace transformations
+}  // namespace actions
+}  // namespace modsecurity
+
+#endif
+
+#endif  // SRC_ACTIONS_TRANSFORMATIONS_UPPER_CASE_H_
