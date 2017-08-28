a2427df27f482c64ea8666dca9552c67d3a68904
Felipe Zimmerle <fcosta@trustwave.com>
2017-08-27 23:39:43 -0300

    fix: ignore .git directory while generating the release file

--- build/release.sh
+++ build/release.sh
@@ -11,7 +11,7 @@ MY_DIR=${PWD##*/}
 ./build.sh
 
 cd ..
-tar --transform "s/^$MY_DIR/$DIR_NAME/" -cvzf $TAR_NAME $MY_DIR
+tar --transform "s/^$MY_DIR/$DIR_NAME/" -cvzf $TAR_NAME --exclude .git $MY_DIR
 
 sha256sum $TAR_NAME > $TAR_NAME.sha256
 gpg --detach-sign -a $TAR_NAME
