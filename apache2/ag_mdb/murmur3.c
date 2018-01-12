62ef8bcc0460b0531e2d368e68fff97c470dbf8b
Peng Cheng <pengc@microsoft.com>
2018-01-12 11:24:16 +0800

    Update FORCE_INLINE defination to support MSVC in the murmur3.c

--- apache2/ag_mdb/murmur3.c
+++ apache2/ag_mdb/murmur3.c
@@ -12,7 +12,9 @@
 //-----------------------------------------------------------------------------
 // Platform-specific functions and macros
 
-#ifdef __GNUC__
+#ifdef _MSC_VER
+#define FORCE_INLINE __forceinline
+#elif defined(__GNUC__)
 #define FORCE_INLINE __attribute__((always_inline)) inline
 #else
 #define FORCE_INLINE inline
