5aa79c17f24b423b6ad108f50ec6f6cb37edadd6
Victor Hora <vhora@trustwave.com>
2018-09-19 19:47:05 -0400

    Add test cases for m.setvar in Lua scripts

---
+++ test/test-cases/data/setvar.lua
@@ -0,0 +1,10 @@
+function main()
+	var = 2;
+	m.setvar("TX.lua_set_var", var);
+	m.setvar("IP.lua_set_var", var);
+	m.setvar("GLOBAL.lua_set_var", var);
+	m.setvar("RESOURCE.lua_set_var", var);
+	m.setvar("SESSION.lua_set_var", var);
+	m.setvar("USER.lua_set_var", var);
+	return nil;
+end
