abab10e5d58410673aaf3715438919d054e511de
Ervin Hegedus <airween@gmail.com>
2019-02-01 22:38:41 +0000

    Fixed data collecting in multipart parsing

--- src/request_body_processor/multipart.cc
+++ src/request_body_processor/multipart.cc
@@ -1077,8 +1077,8 @@ int Multipart::multipart_complete(std::string *error) {
                 name.assign(m->m_filename);
             }
 
-            m_transaction->m_variableFiles.set(m->m_filename,
-                m->m_filename, m->m_filenameOffset);
+            m_transaction->m_variableFiles.set(m->m_name,
+                m->m_name, m->m_nameOffset);
 
             m_transaction->m_variableFilesNames.set(m->m_filename,
                 m->m_filename, m->m_filenameOffset);
