d7d8fc62afca68674c3494425fb08b542e0791a4
Felipe Zimmerle <felipe@zimmerle.org>
2019-02-26 21:59:08 -0300

    Moves rule* headers to src/

--- src/rule_with_actions.cc
+++ src/rule_with_actions.cc
@@ -29,7 +29,7 @@
 #include "modsecurity/modsecurity.h"
 #include "modsecurity/rule_message.h"
 #include "modsecurity/rules_set.h"
-#include "modsecurity/rule_with_actions.h"
+#include "src/rule_with_actions.h"
 #include "src/actions/accuracy.h"
 #include "src/actions/block.h"
 #include "src/actions/capture.h"
@@ -359,10 +359,8 @@ void RuleWithActions::executeTransformations(
     int none = 0;
 
     ModSecString ssin;
-    ssin.assign(in.c_str());
-
-    TransformationResult a = TransformationResult(&ssin);
-    results.push_back(a);
+    ssin.assign(in.c_str(), in.size());
+    results.push_back(TransformationResult(&ssin));
 
 
     std::string path("");
@@ -417,8 +415,8 @@ void RuleWithActions::executeTransformations(
             + std::to_string(results.size()) + \
             " values to be tested.");
     } else {
-        results.pop_front();
-        results.push_back(TransformationResult(&ssin));
+        //results.push_back(TransformationResult(nullptr, ssin));
+        //results.pop_front();
     }
 */
 }
@@ -440,7 +438,7 @@ void RuleWithActions::executeTransformation(
 
 void RuleWithActions::executeTransformation(
     Transaction *transaction,
-    ModSecString in,
+    ModSecString &in,
     TransformationsResults *ret,
     Transformation *transformation) {
 
