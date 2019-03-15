cbf41ce0e6d67aea36337dca6eba136717176018
Felipe Zimmerle <felipe@zimmerle.org>
2019-03-15 01:35:07 -0300

    Removes RuleMessage from action execute signature

--- src/actions/disruptive/deny.h
+++ src/actions/disruptive/deny.h
@@ -33,8 +33,7 @@ class Deny : public Action {
  public:
     explicit Deny(const std::string &action) : Action(action) { }
 
-    bool execute(RuleWithActions *rule, Transaction *transaction,
-        RuleMessage &rm) override;
+    bool execute(RuleWithActions *rule, Transaction *transaction) override;
     bool isDisruptive() override { return true; }
 };
 
