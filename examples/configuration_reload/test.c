baa6dfc52ca9a423ea214f59d87cf793fc9b5e70
Felipe Zimmerle <felipe@zimmerle.org>
2019-03-20 16:30:58 -0300

    Adds first configuration reload example

---
+++ examples/configuration_reload/test.c
@@ -0,0 +1,61 @@
+/*
+ * ModSecurity, http://www.modsecurity.org/
+ * Copyright (c) 2015 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+ *
+ * You may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * If any of the files related to licensing are missing or if you have any
+ * other questions related to licensing please contact Trustwave Holdings, Inc.
+ * directly using the email address security@modsecurity.org.
+ *
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "modsecurity/modsecurity.h"
+#include "modsecurity/rules_set.h"
+
+
+char main_rule_uri[] = "basic_rules.conf";
+
+int main (int argc, char **argv)
+{
+    int ret;
+    const char *error = NULL;
+    ModSecurity *modsec;
+    Transaction *transaction = NULL;
+    RulesSet *rules;
+
+    modsec = msc_init();
+
+    msc_set_connector_info(modsec, "ModSecurity-test v0.0.1-alpha (Simple " \
+        "example on how to use ModSecurity API");
+
+    rules = msc_create_rules_set();
+
+    ret = msc_rules_add_file(rules, main_rule_uri, &error);
+    if (ret < 0) {
+        fprintf(stderr, "Problems loading the rules --\n");
+        fprintf(stderr, "%s\n", error);
+        goto end;
+    }
+    msc_rules_dump(rules);
+
+    printf("Waiting for changes...\n");
+
+    for (;;) {
+    }
+
+end:
+    msc_rules_cleanup(rules);
+    msc_cleanup(modsec);
+
+    return 0;
+}
+
+
