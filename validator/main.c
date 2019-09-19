f0129c630507a3c6ac26276a1389c224aa5f5da9
Vladimir Krivopalov <vlkrivop@microsoft.com>
2019-09-19 14:10:42 -0700

    Validation utility for ModSecurity configuration
    This utility attemps to load ModSecurity configuraiton and reports the
    status. It also outputs to stdout any logs that may be emitted by
    ModSecurity engine during configuration loading.
    
    Signed-off-by: Vladimir Krivopalov <vlkrivop@microsoft.com>

---
+++ validator/main.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "standalone/api.h"
+
+static void print_log(void* obj, int level, char* str)
+{
+    if (str != NULL) {
+        puts(str);
+    }
+}
+
+static void exit_with_error(const char* msg)
+{
+    puts(msg);
+    exit(EXIT_FAILURE);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        exit_with_error("Invalid number of command line arguments");
+    }
+
+    server_rec* modsec_server = modsecInit();
+    if (modsec_server == NULL) {
+        exit_with_error("Failed to create ModSecurity server structure");        
+    }
+    char hostname[] = "localhost";
+    modsec_server->server_hostname = hostname;
+
+    modsecSetLogHook(NULL, print_log);
+    modsecStartConfig();
+    directory_config *config = modsecGetDefaultConfig();
+    if (config == NULL) {
+        exit_with_error("Error creating default config");
+    }
+
+    const char *err = modsecProcessConfig(config, argv[1], NULL);
+    if (err != NULL) {
+        exit_with_error(err);
+    }
+
+    exit(EXIT_SUCCESS);
+}
