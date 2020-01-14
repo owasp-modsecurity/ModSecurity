2e8833733201d9b0d84c5587386443b9c22181d6
Felipe Zimmerle <felipe@zimmerle.org>
2020-01-14 14:40:25 -0300

    Test suite: improvements

--- test/test-suite.sh
+++ test/test-suite.sh
@@ -11,14 +11,30 @@ FILE=${@: -1}
 if [[ $FILE == *"test-cases/regression/"* ]]
 then
     AMOUNT=$(./regression_tests countall ../$FILE)
+    RET=$?
+    if [ $RET -ne 0 ]; then
+        echo ":test-result: SKIP: json is not enabled. (regression/$RET) ../$FILE:$i"
+        exit 0
+    fi
+
     for i in `seq 1 $AMOUNT`; do
         $VALGRIND $PARAM ./regression_tests ../$FILE:$i
-        if [ $? -eq 139 ]; then
-            echo ":test-result: FAIL segfault: ../$FILE:$i"
+        RET=$?
+        if [ $RET -ne 0 ]; then
+            echo ":test-result: FAIL possible segfault/$RET: ../$FILE:$i"
         fi
         echo $VALGRIND $PARAM ./regression_tests ../$FILE:$i
     done;
 else
       $VALGRIND $PARAM ./unit_tests ../$FILE
+      RET=$?
+      if [ $RET -eq 127 ]
+      then
+          echo ":test-result: SKIP: json is not enabled. (unit/$RET) ../$FILE"
+      elif [ $RET -ne 0 ]
+      then
+          echo ":test-result: FAIL possible segfault: (unit/$RET) ../$FILE"
+      fi
 fi
+
 cd - 1> /dev/null
