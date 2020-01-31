357c14000350234beb017ea08f01dc7fed27ed4a
Felipe Zimmerle <felipe@zimmerle.org>
2020-01-31 10:32:37 -0300

    Changens copyright year

--- src/actions/xmlns.h
+++ src/actions/xmlns.h
@@ -1,6 +1,6 @@
 /*
  * ModSecurity, http://www.modsecurity.org/
- * Copyright (c) 2015 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+ * Copyright (c) 2015 - 2020 Trustwave Holdings, Inc. (http://www.trustwave.com/)
  *
  * You may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
--- src/rule_script.h
+++ src/rule_script.h
@@ -1,7 +1,7 @@
 
 /*
  * ModSecurity, http://www.modsecurity.org/
- * Copyright (c) 2015 Trustwave Holdings, Inc. (http://www.trustwave.com/)
+ * Copyright (c) 2015 - 2020 Trustwave Holdings, Inc. (http://www.trustwave.com/)
  *
  * You may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
