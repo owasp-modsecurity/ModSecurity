bb908bd0de7075a1c496898ca471d7c9644951f8
martinhsv <55407942+martinhsv@users.noreply.github.com>
2020-10-21 13:55:46 -0700

    Implement id ranges for ctl:ruleRemoveTargetById

--- headers/modsecurity/transaction.h
+++ headers/modsecurity/transaction.h
@@ -545,6 +545,7 @@ class Transaction : public TransactionAnchoredVariables, public TransactionSecMa
      *
      */
     std::list< std::pair<int, std::string> > m_ruleRemoveTargetById;
+    std::list< std::pair<std::pair<int, int>, std::string> > m_ruleRemoveTargetByIdRange;
 
     /**
      *
