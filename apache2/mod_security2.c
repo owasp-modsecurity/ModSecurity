158890ee5d3f85ea1584921e072a57aff8cbca87
dune73 <email_github.com@tikon.ch>
2022-10-10 22:35:24 +0200

    Make sure ModSecurity runs after mod_maxminddb in phase 1

--- apache2/mod_security2.c
+++ apache2/mod_security2.c
@@ -1668,6 +1668,7 @@ static void register_hooks(apr_pool_t *mp) {
         "mod_breach_realip.c",
         "mod_breach_trans.c",
         "mod_unique_id.c",
+        "mod_maxminddb.c",
         NULL
     };
 
