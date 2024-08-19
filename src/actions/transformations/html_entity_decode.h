8bf4d96e6b3604860586251f098ba5ade3f9b67d
Eduardo Arias <eduar_hte@hotmail.com>
2024-08-19 09:04:42 -0700

    Perform HtmlEntityDecode transformation in-place - Removed inplace helper function from the class, as it's only   referenced by the implementation.

--- src/actions/transformations/html_entity_decode.h
+++ src/actions/transformations/html_entity_decode.h
@@ -26,8 +26,6 @@ class HtmlEntityDecode : public Transformation {
         : Transformation(action) { }
 
     bool transform(std::string &value, const Transaction *trans) const override;
-
-    static int inplace(unsigned char *input, uint64_t input_len);
 };
 
 }  // namespace modsecurity::actions::transformations
