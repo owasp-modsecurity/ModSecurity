c3ab480979ca44b083474ccac3f4c4bd5c004ee9
Ervin Hegedus <airween@gmail.com>
2025-04-26 20:31:43 +0200

    Typo fix.
    Co-authored-by: Max Leske <250711+theseion@users.noreply.github.com>

--- apache2/msc_xml.c
+++ apache2/msc_xml.c
@@ -241,7 +241,7 @@ int xml_process_chunk(modsec_rec *msr, const char *buf, unsigned int size, char 
             msr->txcfg->parse_xml_into_args != MSC_XML_ARGS_ONLYARGS) {
             xmlParseChunk(msr->xml->parsing_ctx, buf, size, 0);
             if (msr->xml->parsing_ctx->wellFormed != 1) {
-                *error_msg = apr_psprintf(msr->mp, "XML: Failed parsing document.");
+                *error_msg = apr_psprintf(msr->mp, "XML: Failed to parse document.");
                 return -1;
             }
         }
