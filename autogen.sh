7d5ad80f0d24c3e31937e4ee00acabdbcf2d6819
Hegedüs Dávid Marcell <david@arxnet.hu>
2026-01-29 16:40:55 +0100

    fixing Makefile.in missing

--- autogen.sh
+++ autogen.sh
@@ -9,7 +9,7 @@ rm -rf autom4te.cache
 rm -f aclocal.m4
 case `uname` in Darwin*) glibtoolize --force --copy ;;
   *) libtoolize --force --copy ;; esac
-autoreconf --install
+autoreconf --install --force
 autoheader
 automake --add-missing --foreign --copy --force-missing
 autoconf --force
