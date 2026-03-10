bf4fff97a081ac6f57d4aaa19b7d316276fe0331
Ervin Hegedus <airween@gmail.com>
2026-03-10 22:09:35 +0100

    Remove redundant initialization

--- src/transaction.cc
+++ src/transaction.cc
@@ -527,7 +527,7 @@ int Transaction::addRequestHeader(const std::string& key,
 
             // find the first '='
             pos = c.find_first_of("=", 0);
-            std::string ckey = "";
+            std::string ckey;
             std::string cval = "";
 
             // if the cookie doesn't contains '=', its just a key
