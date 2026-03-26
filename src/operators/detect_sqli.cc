38d93911ae0da70690e9962eaadcd8ab1cbafae2
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-03-26 21:12:08 +0100

    Stabilize detectSQLi/XSS regression coverage for full test run

--- src/operators/detect_sqli.cc
+++ src/operators/detect_sqli.cc
@@ -20,6 +20,7 @@
 
 #include "src/operators/operator.h"
 #include "libinjection/src/libinjection.h"
+#include "libinjection/src/libinjection_error.h"
 
 namespace modsecurity {
 namespace operators {
@@ -28,32 +29,52 @@ namespace operators {
 bool DetectSQLi::evaluate(Transaction *t, RuleWithActions *rule,
     const std::string& input, RuleMessage &ruleMessage) {
     char fingerprint[8];
-    int issqli;
+    injection_result_t sqli_result;
+    bool is_match = false;
 
-    issqli = libinjection_sqli(input.c_str(), input.length(), fingerprint);
+    sqli_result = libinjection_sqli(input.c_str(), input.length(), fingerprint);
 
     if (!t) {
         goto tisempty;
     }
 
-    if (issqli) {
-        t->m_matched.push_back(fingerprint);
-        ms_dbg_a(t, 4, "detected SQLi using libinjection with " \
-            "fingerprint '" + std::string(fingerprint) + "' at: '" +
-            input + "'");
-        if (rule && rule->hasCaptureAction()) {
-            t->m_collections.m_tx_collection->storeOrUpdateFirst(
-                "0", std::string(fingerprint));
-            ms_dbg_a(t, 7, "Added DetectSQLi match TX.0: " + \
-                std::string(fingerprint));
-        }
-    } else {
-        ms_dbg_a(t, 9, "detected SQLi: not able to find an " \
-            "inject on '" + input + "'");
+    switch (sqli_result) {
+        case LIBINJECTION_RESULT_TRUE:
+            is_match = true;
+            t->m_matched.push_back(fingerprint);
+            ms_dbg_a(t, 4, "detected SQLi using libinjection with " \
+                "fingerprint '" + std::string(fingerprint) + "' at: '" +
+                input + "'");
+            if (rule && rule->hasCaptureAction()) {
+                t->m_collections.m_tx_collection->storeOrUpdateFirst(
+                    "0", std::string(fingerprint));
+                ms_dbg_a(t, 7, "Added DetectSQLi match TX.0: " + \
+                    std::string(fingerprint));
+            }
+            break;
+        case LIBINJECTION_RESULT_ERROR:
+            is_match = true;
+            ms_dbg_a(t, 4, "libinjection parser error during SQLi "
+                "analysis; treating as match (fail-safe). Input: '" + input + "'");
+            if (rule && rule->hasCaptureAction()) {
+                t->m_collections.m_tx_collection->storeOrUpdateFirst(
+                    "0", std::string(input));
+                ms_dbg_a(t, 7, "Added DetectSQLi error input TX.0: " + \
+                    std::string(input));
+            }
+            break;
+        case LIBINJECTION_RESULT_FALSE:
+            ms_dbg_a(t, 9, "detected SQLi: not able to find an " \
+                "inject on '" + input + "'");
+            break;
     }
 
 tisempty:
-    return issqli != 0;
+    if (t == nullptr) {
+        is_match = sqli_result == LIBINJECTION_RESULT_TRUE
+            || sqli_result == LIBINJECTION_RESULT_ERROR;
+    }
+    return is_match;
 }
 
 
--- src/operators/detect_xss.cc
+++ src/operators/detect_xss.cc
@@ -19,6 +19,7 @@
 
 #include "src/operators/operator.h"
 #include "libinjection/src/libinjection.h"
+#include "libinjection/src/libinjection_error.h"
 
 
 namespace modsecurity {
@@ -27,25 +28,46 @@ namespace operators {
 
 bool DetectXSS::evaluate(Transaction *t, RuleWithActions *rule,
     const std::string& input, RuleMessage &ruleMessage) {
-    int is_xss;
-
-    is_xss = libinjection_xss(input.c_str(), input.length());
+    injection_result_t xss_result = libinjection_xss(input.c_str(),
+        input.length());
+    bool is_match = false;
 
     if (t) {
-        if (is_xss) {
-            ms_dbg_a(t, 5, "detected XSS using libinjection.");
-            if (rule && rule->hasCaptureAction()) {
-                t->m_collections.m_tx_collection->storeOrUpdateFirst(
-                    "0", std::string(input));
-                ms_dbg_a(t, 7, "Added DetectXSS match TX.0: " + \
-                    std::string(input));
-            }
-        } else {
-            ms_dbg_a(t, 9, "libinjection was not able to " \
-                "find any XSS in: " + input);
-            }
+        switch (xss_result) {
+            case LIBINJECTION_RESULT_TRUE:
+                is_match = true;
+                ms_dbg_a(t, 5, "detected XSS using libinjection.");
+                if (rule && rule->hasCaptureAction()) {
+                    t->m_collections.m_tx_collection->storeOrUpdateFirst(
+                        "0", std::string(input));
+                    ms_dbg_a(t, 7, "Added DetectXSS match TX.0: " + \
+                        std::string(input));
+                }
+                break;
+            case LIBINJECTION_RESULT_ERROR:
+                is_match = true;
+                ms_dbg_a(t, 4, "libinjection parser error during XSS "
+                    "analysis; treating as match (fail-safe). Input: " + input);
+                if (rule && rule->hasCaptureAction()) {
+                    t->m_collections.m_tx_collection->storeOrUpdateFirst(
+                        "0", std::string(input));
+                    ms_dbg_a(t, 7, "Added DetectXSS error input TX.0: " + \
+                        std::string(input));
+                }
+                break;
+            case LIBINJECTION_RESULT_FALSE:
+                ms_dbg_a(t, 9, "libinjection was not able to " \
+                    "find any XSS in: " + input);
+                break;
+        }
     }
-    return is_xss != 0;
+
+    if (t == nullptr) {
+        is_match = xss_result == LIBINJECTION_RESULT_TRUE
+            || xss_result == LIBINJECTION_RESULT_ERROR;
+    }
+
+    return is_match;
 }
 
 
