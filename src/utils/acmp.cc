66c338e1fce5a6294bdd790aa1b7312a9f4bd19c
Ervin Hegedus <airween@gmail.com>
2026-04-12 22:31:13 +0200

    Remove unnecessary memory allocation

--- src/utils/acmp.cc
+++ src/utils/acmp.cc
@@ -387,7 +387,7 @@ if (parser->is_active != 0) return -1;
             child->pattern = (char *)"";
             child->letter = letter;
             child->depth = i;
-            child->text = (char *)calloc(1, length + 2);
+            child->text = (char *)calloc(1, i + 2);
             /* ENH: Check alloc succeded */
             for (j = 0; j <= i; j++) child->text[j] = pattern[j];
         }
@@ -395,7 +395,7 @@ if (parser->is_active != 0) return -1;
             if (child->is_last == 0) {
                 parser->dict_count++;
                 child->is_last = 1;
-                child->pattern = (char *)calloc(1, length + 2);
+                child->pattern = (char *)calloc(1, length + 1);
                 /* ENH: Check alloc succeded */
                 memcpy(child->pattern, pattern, length);
                 child->pattern[length] = '\0';
