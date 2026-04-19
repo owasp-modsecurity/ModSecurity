9b33b2eb5d2bb3dc5bc8c6e8ff60f50d33e7993b
Ervin Hegedus <airween@gmail.com>
2026-04-19 16:03:40 +0200

    Change conditions' order; cast isdigit()'s argument

--- src/request_body_processor/multipart.cc
+++ src/request_body_processor/multipart.cc
@@ -362,7 +362,7 @@ int Multipart::parse_content_disposition(const char *c_d_value, int offset) {
             const char* start_of_filename = p;
             while ((*p != '\0') && (*p != ';')) {
                 if (*p == '%') {
-                    if ((*(p+1) == '\0') || (!isxdigit(*(p+1))) || (*(p+2) == '\0') || (!isxdigit(static_cast<unsigned char>(*(p+2))))) {
+                    if ((*(p+1) == '\0') || (*(p+2) == '\0') || (!isxdigit(static_cast<unsigned char>(*(p+1)))) || (!isxdigit(static_cast<unsigned char>(*(p+2))))) {
                         return -18;
                     }
                     p += 3;
