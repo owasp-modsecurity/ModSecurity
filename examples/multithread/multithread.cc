fd99217128f54625ea267dc11792e1f87ea7e536
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-04-25 05:53:04 +0200

    Add argument handling in main function

--- examples/multithread/multithread.cc
+++ examples/multithread/multithread.cc
@@ -62,6 +62,8 @@ static int count_open_fds(void) {
 #endif
 
 int main (int argc, char *argv[]) {
+    const char * const *args = argv;
+    
     auto modsec = std::make_unique<modsecurity::ModSecurity>();
     modsec->setConnectorInformation("ModSecurity-test v0.0.1-alpha (Simple " \
         "example on how to use ModSecurity API");
