f1329ffbc1f038fb698f6e3d3b77942d370b0f8a
Easton97-Jens <66330090+Easton97-Jens@users.noreply.github.com>
2026-04-25 16:24:21 +0200

    Update inspect_file.cc

--- src/operators/inspect_file.cc
+++ src/operators/inspect_file.cc
@@ -187,7 +187,7 @@ bool InspectFile::evaluate(Transaction *transaction, const std::string &str) {
 
         std::vector<char *> argv;
         argv.reserve(argv_storage.size() + 1);
-        for (std::string &arg : argv_storage) {
+        for (const std::string &arg : argv_storage) {
             argv.push_back(arg.data());
         }
         argv.push_back(nullptr);
