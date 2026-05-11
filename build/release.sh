owasp-modsecurity/ModSecurity

commit 04f7009980d6715749de697710a640fd54a32789

Adds a simple release script

build/release.sh
@@ -1,0 +1,21 @@
+#!/bin/bash
+
+git clean -xfdi
+git submodule foreach --recursive git clean -xfdi
+
+VERSION=`git describe --tags`
+DIR_NAME="modsecurity-$VERSION"
+TAR_NAME="modsecurity-$VERSION.tar.gz"
+
+MY_DIR=${PWD##*/}
+./build.sh
+
+cd ..
+tar --transform "s/^$MY_DIR/$DIR_NAME/" -cvzf $TAR_NAME $MY_DIR
+
+sha256sum $TAR_NAME > $TAR_NAME.sha256
+gpg --detach-sign -a $TAR_NAME
+
+cd -
+echo $TAR_NAME ": done."
+

commit d886345f6a59062bd208bac5e4bcc3c2661676ac

Exlude .gitignore from release tarballs
The `.gitignore` files should not be included in the release tarballs
because they hide required files and can conflict with existing
`.gitignore` files when the tarball is extracted into a git repository.

Closes: https://github.com/owasp-modsecurity/ModSecurity/issues/3559
Fixes: 04f70099 ("Adds a simple release script")

build/release.sh
@@ -11,7 +11,7 @@ MY_DIR=${PWD##*/}
 ./build.sh
 
 cd ..
-tar --transform "s/^$MY_DIR/$DIR_NAME/" -cvzf $TAR_NAME --exclude .git $MY_DIR
+tar --transform "s/^$MY_DIR/$DIR_NAME/" -cvzf $TAR_NAME --exclude .git --exclude .gitignore $MY_DIR
 
 sha256sum $TAR_NAME > $TAR_NAME.sha256
 gpg --detach-sign -a $TAR_NAME