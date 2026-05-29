5904a75f527be643fb4b67014372d817fc9ebee9
Hiroaki Nakamura <hnakamur@gmail.com>
2026-05-29 16:01:09 +0900

    Return 413 when request which is bigger than SecRequestBodyLimit is rejected

--- src/transaction.cc
+++ src/transaction.cc
@@ -990,7 +990,7 @@ int Transaction::appendRequestBody(const unsigned char *buf, size_t len) {
                     intervention::free(&m_it);
                     m_it.log = strdup("Request body limit is marked to " \
                             "reject the request");
-                    m_it.status = 403;
+                    m_it.status = 413;
                     m_it.disruptive = true;
                 } else {
                     ms_dbg(5, "Not rejecting the request as the engine is " \
