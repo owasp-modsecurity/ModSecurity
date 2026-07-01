dcf0a1b650781eed4b28b53c7e7f71584eca41d0
Ervin Hegedus <airween@gmail.com>
2026-07-01 20:53:22 +0200

    chore: fix cppcheck issues with v2.21.0

--- standalone/filters.c
+++ standalone/filters.c
@@ -63,10 +63,11 @@ void init_filter_tables()
 	}
 }
 
+
 AP_DECLARE(ap_filter_rec_t *) ap_register_input_filter(const char *name,
-                                          ap_in_filter_func filter_func,
-                                          ap_init_filter_func filter_init,
-                                          ap_filter_type ftype)
+                                          ap_in_filter_func filter_func, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                                          ap_init_filter_func filter_init, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                                          ap_filter_type ftype) // cppcheck-suppress funcArgNamesDifferentUnnamed
 {
 	ap_filter_rec_t *f;
 
@@ -85,9 +86,9 @@ AP_DECLARE(ap_filter_rec_t *) ap_register_input_filter(const char *name,
 }
 
 AP_DECLARE(ap_filter_rec_t *) ap_register_output_filter(const char *name,
-                                            ap_out_filter_func filter_func,
-                                            ap_init_filter_func filter_init,
-                                            ap_filter_type ftype)
+                                            ap_out_filter_func filter_func, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                                            ap_init_filter_func filter_init, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                                            ap_filter_type ftype) // cppcheck-suppress funcArgNamesDifferentUnnamed
 {
 	ap_filter_rec_t *f;
 	
--- standalone/hooks.c
+++ standalone/hooks.c
@@ -50,16 +50,16 @@ link##_DECLARE(void) ns##_hook_##name(ns##_HOOK_##name##_t *pf, \
 #define DECLARE_HOOK(ret,name,args) \
 	DECLARE_EXTERNAL_HOOK(ap,AP,ret,name,args)
 
-DECLARE_HOOK(int,pre_config,(apr_pool_t *pconf,apr_pool_t *plog, apr_pool_t *ptemp))
-DECLARE_HOOK(int,post_config,(apr_pool_t *pconf,apr_pool_t *plog, apr_pool_t *ptemp,server_rec *s))
-DECLARE_HOOK(void,child_init,(apr_pool_t *pchild, server_rec *s))
-DECLARE_HOOK(int,process_connection,(conn_rec *c))
-DECLARE_HOOK(int,post_read_request,(request_rec *r))
-DECLARE_HOOK(int,fixups,(request_rec *r))
-DECLARE_HOOK(void, error_log, (const char *file, int line, int level,
-                       apr_status_t status, const server_rec *s,
-                       const request_rec *r, apr_pool_t *pool,
-                       const char *errstr))
-DECLARE_HOOK(int,log_transaction,(request_rec *r))
-DECLARE_HOOK(void,insert_filter,(request_rec *r))
-DECLARE_HOOK(void,insert_error_filter,(request_rec *r))
+DECLARE_HOOK(int,pre_config,(apr_pool_t *pconf,apr_pool_t *plog, apr_pool_t *ptemp)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(int,post_config,(apr_pool_t *pconf,apr_pool_t *plog, apr_pool_t *ptemp,server_rec *s)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(void,child_init,(apr_pool_t *pchild, server_rec *s)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(int,process_connection,(conn_rec *c)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(int,post_read_request,(request_rec *r)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(int,fixups,(request_rec *r)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(void, error_log, (const char *file, int line, int level, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                       apr_status_t status, const server_rec *s, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                       const request_rec *r, apr_pool_t *pool, // cppcheck-suppress funcArgNamesDifferentUnnamed
+                       const char *errstr)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(int,log_transaction,(request_rec *r)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(void,insert_filter,(request_rec *r)) // cppcheck-suppress funcArgNamesDifferentUnnamed
+DECLARE_HOOK(void,insert_error_filter,(request_rec *r)) // cppcheck-suppress funcArgNamesDifferentUnnamed
