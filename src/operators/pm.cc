5de262bdd7122a1b900329a553e5352c6887b674
chenuduss <gruzino.abhaz@gmail.com>
2026-09-15 11:48:33 +0300

    pointer check

--- src/operators/pm.cc
+++ src/operators/pm.cc
@@ -165,7 +165,7 @@ bool Pm::init(const std::string &file, std::string *error) {
 
     while (m_p->is_failtree_done == 0) {
         if (acmp_prepare(m_p) != 1) {
-            error->assign("Failed to prepare pattern matcher.");
+            if (error) error->assign("Failed to prepare pattern matcher.");
             return false;
         }
     }
